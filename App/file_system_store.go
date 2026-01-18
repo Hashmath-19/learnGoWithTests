23a5a8fb311e537f2113ba5400a53ec3b0e2121d
Hash <84575247+Hashmath-19@users.noreply.github.com>
2026-01-18 13:37:32 +0530

    Add code for Go App (#2)

---
+++ App/file_system_store.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
+// file_system_store.go
+type FileSystemPlayerStore struct {
+	database *json.Encoder
+	league   League
+}
+
+func NewFileSystemPlayerStore(file *os.File) *FileSystemPlayerStore {
+	file.Seek(0, io.SeekStart)
+	league, _ := NewLeague(file)
+
+	return &FileSystemPlayerStore{
+		database: json.NewEncoder(&tape{file}),
+		league:   league,
+	}
+}
+
+func (f *FileSystemPlayerStore) GetLeague() League {
+	return f.league
+}
+
+// file_system_store.go
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+
+	player := f.league.Find(name)
+
+	if player != nil {
+		return player.Wins
+	}
+
+	return 0
+}
+
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	player := f.league.Find(name)
+
+	if player != nil {
+		player.Wins++
+	} else {
+		f.league = append(f.league, Player{name, 1})
+	}
+
+	f.database.Encode(f.league)
+}
---
+++ blogrenderer/go.mod
@@ -0,0 +1,3 @@
+module github.com/Hashmath-19/blogrenderer
+
+go 1.23.5
